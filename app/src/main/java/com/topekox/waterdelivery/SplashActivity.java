05cedded5b01794441ed2843669559d2c6a30ba4
Ucup Topekox <ucup.topekox@gmail.com>
2022-06-04 14:57:53 +0800

    Initial commit

---
+++ app/src/main/java/com/topekox/waterdelivery/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.topekox.waterdelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide(); // Action Bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); // Fullscreen / hide status bar
+
+        TextView textView = findViewById(R.id.text_splashscreen);
+        textView.animate().translationX(1000).setDuration(1000).setStartDelay(2500);
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
